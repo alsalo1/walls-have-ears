58ccec29aa7c9449d9edd8552cc1c618ab8ab1a3
Robert Williamson <robert.williamson@here.com>
2017-03-16 10:05:55 +0100

    Load the image into the canvas.

--- src/client-js/roomCanvas.js
+++ src/client-js/roomCanvas.js
@@ -1,5 +1,59 @@
-
-
-function getRootNode() {
-    return document.getElementById("roomCanvas");
+var loaded = {
+    img: false,
+    script: false
 }
+
+function ready() {
+    return loaded.img && loaded.script;
+}
+
+function setup() {
+    window.onload = () => {
+        getImg();
+    }
+    createContent();
+}
+
+function createContent() {
+    loaded.script = document.getElementById('script');
+
+    loaded.script.onload = () => {
+        getImg();
+    };
+
+    loaded.script.onreadystatechange = () => {
+        getImg();
+    };
+}
+
+function paint() {
+    if (!ready()) {
+        return;
+    }
+
+    var canvas = document.getElementById('roomCanvas');
+    var img = loaded.img;
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0,0);
+}
+
+function getImg() {
+    if (!loaded.img) {
+        var newImg = new Image();
+
+        newImg.onload = function() {
+            loaded.img = newImg;
+
+            paint();
+        }
+
+        newImg.src = 'img/floorplan.png';
+    } else {
+        paint();
+    }
+}
+
+setup();
